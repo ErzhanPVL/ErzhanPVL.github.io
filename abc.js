a1d7a4778bba1e5879b60aca47f0311393c7f7c7
ErzhanPVL <44885371+ErzhanPVL@users.noreply.github.com>
2024-03-18 11:39:22 +0500

    Update abc.js

--- abc.js
+++ abc.js
@@ -37237,7 +37237,7 @@ function checkPrize() {
     } else if(winners1.hasOwnProperty(enteredNumber) && enteredNumber!==""){
 
         var prize = winners1[enteredNumber];
-        popup.innerText="Позравляем! Вы выиграли " + prize;
+        popup.innerText="Поздравляем! Вы выиграли " + prize;
         //alert("Позравляем! Вы выиграли " + prize);
     }
     
